c73aaad04203e2bf5814ae50cd21679b4ed24155
fidhayasmin <145666933+fidhayasmin@users.noreply.github.com>
2024-03-18 14:40:53 +0530

    Add files via upload

---
+++ prime.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for num in {50..100}
+do
+	is_prime=true
+	for (( i=2; i<num; i++ ))
+	do
+		if [ $(($num%$i)) -eq 0 ]
+		then
+			is_prime=false
+			break
+		fi
+	done
+	if $is_prime 
+	then
+		echo $num
+	fi
+done
